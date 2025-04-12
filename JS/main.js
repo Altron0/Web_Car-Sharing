21f9c67e155c6ff2d61f985e5452eb2fd866f5b5
MAKS <mitenin101@mail.ru>
2025-04-12 11:52:17 +0300

    Полностью сверстал адаптивную верстку

--- JS/main.js
+++ JS/main.js
@@ -21,7 +21,31 @@ $(function () {
         dots: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true
+        autoplay: true,
+
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                },
+            }
+
+        ]
     })
 
     $('.menu__bth').on('click', function(){
